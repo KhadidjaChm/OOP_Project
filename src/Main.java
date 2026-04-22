ffb51d6437fc0dfe54a576705d293c11ebb1a2c0
KhadidjaChm <khdidjachami@gmail.com>
2026-04-22 20:26:40 +0100

    Initial commit with models

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,55 @@
 import models.*;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
 
-        Student s = new Student(1, "Khadidja");
-        Company c = new Company(1, "Google");
-        InternshipOffer o = new InternshipOffer(1, "Backend Intern", "Java + Spring", c);
-        Application app = new Application(1, s, o);
+        // 1) Create a company
+        Company c = new Company(
+                1,
+                "TechCorp",
+                "Algiers",
+                "contact@techcorp.com",
+                "0550 00 00 00",
+                "IT"
+        );
 
-        System.out.println(app.getStatus()); // Should print PENDING
+        // 2) Create an internship offer
+        InternshipOffer offer = new InternshipOffer(
+                10,
+                "Java Developer Intern",
+                "Work on backend systems",
+                "Software Development",
+                "Algiers",
+                "Java, OOP, Git",
+                12,
+                0.0,
+                LocalDate.of(2026, 6, 1),
+                c
+        );
+
+        // 3) Create a student
+        Student s = new Student(
+                100,
+                "Khadidja",
+                "khadidja@example.com",
+                "1234",
+                "0551 11 11 11",
+                2020123456,
+                "Computer Science",
+                3,
+                "UNI123"
+        );
+
+        // 4) Create an application
+        Application app = new Application(500, s, offer);
+
+        // 5) Print everything to check
+        System.out.println("=== TEST MODELS ===");
+        System.out.println("Company: " + c.getName());
+        System.out.println("Offer: " + offer.getTitle());
+        System.out.println("Student: " + s.getFullName());
+        System.out.println("Application Status: " + app.getStatus());
+        System.out.println("Application Date: " + app.getDateApplied());
     }
 }
--- src/models/Admin.java
+++ src/models/Admin.java
@@ -1,7 +1,20 @@
 package models;
+//Represnts the system administrator
+//Has special permissions to pre-validate applications
+public class Admin extends User {
+    private String adminCode; // Secret admin code for verification
+    public Admin(int id, String fullName, String email, String password, String phoneNumber, String adminCode ){
+        super(id, fullName, email , password , phoneNumber);
+        this.adminCode = adminCode;
 
-public class Admin extends User{
-    public Admin(int id , String name){
-        super(id,name);
+    }
+    //Getters & Setters
+
+    public String getAdminCode() {
+        return adminCode;
+    }
+
+    public void setAdminCode(String adminCode) {
+        this.adminCode = adminCode;
     }
 }
